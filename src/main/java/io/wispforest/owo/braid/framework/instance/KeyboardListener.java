f249a8c961cbcae2336950d563e1a6ff9efa0fbe
glisco <glisco03@web.de>
2025-07-14 02:38:10 +0200

    (braid) mostly port improved `TextInput` and `EditableText` implementations

--- src/main/java/io/wispforest/owo/braid/framework/instance/KeyboardListener.java
+++ src/main/java/io/wispforest/owo/braid/framework/instance/KeyboardListener.java
@@ -1,18 +1,39 @@
 package io.wispforest.owo.braid.framework.instance;
 
-import io.wispforest.owo.braid.core.KeyModifiers;
-
 public interface KeyboardListener {
-    default boolean onKeyDown(int keyCode, KeyModifiers modifiers) {
+    default boolean onKeyDown(int keyCode, int modifiers) {
         return false;
     }
-    default boolean onKeyUp(int keyCode, KeyModifiers modifiers) {
+    default boolean onKeyUp(int keyCode, int modifiers) {
         return false;
     }
-    default boolean onChar(int charCode, KeyModifiers modifiers) {
+    default boolean onChar(int charCode, int modifiers) {
         return false;
     }
 
     default void onFocusGained() {}
     default void onFocusLost() {}
+
+    default void requestFocus() {
+        WidgetInstance<?> thisInstance;
+
+        try {
+            thisInstance = (WidgetInstance<?>) this;
+        } catch (ClassCastException e) {
+            throw new ThisOneIsDefinitelyOnYou(e);
+        }
+
+        var host = thisInstance.host();
+        if (host != null) {
+            host.moveFocusTo(this);
+        } else {
+            WidgetInstance.addPostAttachCallback(thisInstance, () -> thisInstance.host().moveFocusTo(this));
+        }
+    }
+}
+
+class ThisOneIsDefinitelyOnYou extends RuntimeException {
+    public ThisOneIsDefinitelyOnYou(Throwable cause) {
+        super(cause);
+    }
 }
