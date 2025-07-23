b229f2a38289b209d8b1b831889dedf642da8be9
Lily Mae <glisco03@web.de>
2025-07-24 00:50:54 +0200

    Merge pull request #409 from chyzman/chyz-works-on-braid
    Chyz works on braid part 5

--- src/main/java/io/wispforest/owo/mixin/ui/layers/MouseMixin.java
+++ src/main/java/io/wispforest/owo/mixin/ui/layers/MouseMixin.java
@@ -1,5 +1,9 @@
 package io.wispforest.owo.mixin.ui.layers;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import io.wispforest.owo.braid.core.BraidScreen;
 import io.wispforest.owo.ui.layers.Layers;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.gui.screen.Screen;
@@ -25,4 +29,16 @@ public class MouseMixin {
         if (handled) ci.cancel();
     }
 
+    // TODO: port
+//    @WrapOperation(method = "onMouseButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;mouseClicked(DDI)Z"))
+//    private boolean passModifiersToBraid$mouseClicked(Screen screen, double mouseX, double mouseY, int button, Operation<Boolean> original, @Local(argsOnly = true, ordinal = 2) int modifiers) {
+//        if (screen instanceof BraidScreen braidScreen) return braidScreen.mouseClicked(mouseX, mouseY, button, modifiers);
+//        return original.call(screen, mouseX, mouseY, button);
+//    }
+//
+//    @WrapOperation(method = "onMouseButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;mouseReleased(DDI)Z"))
+//    private boolean passModifiersToBraid$mouseReleased(Screen screen, double mouseX, double mouseY, int button, Operation<Boolean> original, @Local(argsOnly = true, ordinal = 2) int modifiers) {
+//        if (screen instanceof BraidScreen braidScreen) return braidScreen.mouseReleased(mouseX, mouseY, button, modifiers);
+//        return original.call(screen, mouseX, mouseY, button);
+//    }
 }
